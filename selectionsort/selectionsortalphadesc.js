ee1b1374deda77dc85fc31beeb812313f2f40c9d
Ankit <ankitsaini8847@gmail.com>
2023-01-22 22:18:22 +0530

    secondcommit

--- selectionsort/selectionsortalphadesc.js
+++ selectionsort/selectionsortalphadesc.js
@@ -1,17 +1,17 @@
-let name1="ankitsaini";
-let a=name1.split('');
+let name1 = "ankitsaini";
+let a = name1.split("");
 
-let temp=0,min;
-for(let i=0;i<a.length;i++){
-  min=i;
-  for(let j=i+1;j<a.length;j++){
-    if(a[j]>a[min]){
-      min=j;
+let temp = 0,
+  min;
+for (let i = 0; i < a.length; i++) {
+  min = i;
+  for (let j = i + 1; j < a.length; j++) {
+    if (a[j] > a[min]) {
+      min = j;
     }
-  
   }
-    temp=a[i]
-    a[i]=a[min]
-    a[min]=temp;
+  temp = a[i];
+  a[i] = a[min];
+  a[min] = temp;
 }
-console.log(a.join(''))
+console.log(a.join(""));
--- selectionsort/selectionsortasc.js
+++ selectionsort/selectionsortasc.js
@@ -1,17 +1,17 @@
-let a=[38,52,9,18,6,62,13]
-let temp=0,min;
-for(let i=0;i<a.length;i++){
-  min=i;
-  for(let j=i+1;j<a.length;j++){
-    if(a[j]<a[min]){
-      min=j;
+let a = [38, 52, 9, 18, 6, 62, 13];
+let temp = 0,
+  min;
+for (let i = 0; i < a.length; i++) {
+  min = i;
+  for (let j = i + 1; j < a.length; j++) {
+    if (a[j] < a[min]) {
+      min = j;
     }
-  
   }
-    temp=a[i]
-    a[i]=a[min]
-    a[min]=temp;
+  temp = a[i];
+  a[i] = a[min];
+  a[min] = temp;
 }
-for(let t of a){
-console.log(t)
+for (let t of a) {
+  console.log(t);
 }
