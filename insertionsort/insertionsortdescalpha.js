559adc7fdfc58dc346c7d3a191146f8c7759c8ae
Ankit <ankitsaini8847@gmail.com>
2023-01-24 13:04:07 +0530

    commit3

---
+++ insertionsort/insertionsortdescalpha.js
@@ -0,0 +1,16 @@
+let a = "ankitsaini";
+let arr=a.split('')
+
+
+//insertion sort
+for (let i = 1; i < arr.length; i++) {
+  let current = arr[i];
+  let j = i - 1;
+  while (j >= 0 && arr[j] < current) {
+    //Keep swapping
+    arr[j + 1] = arr[j];
+    j--;
+  }
+  arr[j + 1] = current;
+}
+console.log(arr.join(''));
--- selectionsort/selectionsortdesc.js
+++ selectionsort/selectionsortdesc.js
@@ -1,16 +1,16 @@
 let a = [38, 52, 9, 18, 6, 62, 13];
 let temp = 0,
-  min;
+  max;
 for (let i = 0; i < a.length; i++) {
-  min = i;
+  max = i;
   for (let j = i + 1; j < a.length; j++) {
-    if (a[j] > a[min]) {
-      min = j;
+    if (a[j] > a[max]) {
+      max = j;
     }
   }
   temp = a[i];
-  a[i] = a[min];
-  a[min] = temp;
+  a[i] = a[max];
+  a[max] = temp;
 }
 for (let t of a) {
   console.log(t);
